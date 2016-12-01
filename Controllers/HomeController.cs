de86668ec804f01366e9526e5cdccc6b8242265c
Justin Middler <jusso15@gmail.com>
2016-12-01 14:19:22 +1100

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Movies.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Movies.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+
+        private readonly ApplicationDbContext _context;
+
+        public HomeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [AllowAnonymous]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        [AllowAnonymous]
+        public IActionResult Error()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        public IActionResult Forbidden() 
+        {
+            return View();
+        }
+
+        public async Task <IActionResult> Movies()
+        {
+            var movies = from m in _context.Movies
+                         orderby m.Title ascending
+                         select m;
+
+            return View(await movies.ToListAsync());
+        }
+    }
+}
