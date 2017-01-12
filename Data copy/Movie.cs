9bea0027be765b80d4d09b014d0c04052dbf51ad
Justin Middler <jusso15@gmail.com>
2017-01-12 20:06:12 +1100

    Removed validation check on Model State

---
+++ Data copy/Movie.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Movies.Data
+{
+    public class Movie
+    {
+        public int id { get; set; }
+        public string Title { get; set; }
+        [DisplayName("Main Actor")]
+        public string MainActor { get; set; }
+        public string Genre { get; set; }
+        [DisplayName("Release Date")]
+        public DateTime ReleaseDate { get; set; }
+        public decimal Worth { get; set; }
+        public string imageUrl { get; set; }
+
+    }
+}
